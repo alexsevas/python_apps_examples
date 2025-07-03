f263aa2c9504c2b48398b756a48ea70a8b5d4672
alexsevas <mralexrelax@gmail.com>
2025-07-03 10:54:47 +0300

    Add comments

--- PIP/playwright/page_screenshot.py
+++ PIP/playwright/page_screenshot.py
@@ -1,12 +1,14 @@
-# conda activate allpy310
+# conda activate browseusep311 (установлено все: chromium, webkit, firefox; командой: playwright install)
+# conda activate allpy310 (только chromium, установлен командой: playwright install chromium)
 
+# Пример использования playwright с chromium
 from playwright.sync_api import sync_playwright
 
 playwright = sync_playwright().start()
 
 browser = playwright.chromium.launch()
 page = browser.new_page()
-page.goto("https://ya.ru/")
+page.goto("http://playwright.dev")
 page.screenshot(path="data/example1.png")
 browser.close()
 
