2574ba5261b5c479732d521524ed570d54a49403
alexsevas <mralexrelax@gmail.com>
2025-07-16 12:08:44 +0300

    Add pyvista_demos.py

---
+++ PIP/dash/pyvista_demos.py
@@ -0,0 +1,9 @@
+from pyvista import demos
+from pyvista import examples
+
+demos.plot_logo()
+demos.plot_wave()
+demos.plot_beam()
+demos.plot_glyphs()
+demos.plot_ants_plane()
+demos.plot_datasets()
