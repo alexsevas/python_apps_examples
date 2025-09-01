79335817b40c9fedfb15e911a66894bec4d41240
alexsevas <mralexrelax@gmail.com>
2025-09-01 14:14:28 +0300

    Add pprint, comments

--- CV/OCR_pytesseract/OCR_passport_IMG2TXT.py
+++ CV/OCR_pytesseract/OCR_passport_IMG2TXT.py
@@ -1,18 +1,23 @@
 # conda activate OCRpy310
 # Извлечение данных из сканов паспортов, чеков, справок
+# !!! вывод текста на картинку средствами CV2 - не работает с кириллицей
 
 import cv2
 import pytesseract
+import pprint
 
 # Путь к изображению
-img = cv2.imread("D:\\Projects\\Data\\OCR\\m_passport.jpg")
+img = cv2.imread("D:\\Projects\\Data\\OCR\\test_med_ocr.jpg")
+
 
 # Предобработка (можно настроить под тип документа)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
 
 # Распознавание + координаты
-data = pytesseract.image_to_data(gray, lang="rus", output_type=pytesseract.Output.DICT)
+#data = pytesseract.image_to_data(gray, lang="rus", output_type=pytesseract.Output.DICT)
+#data = pytesseract.image_to_data(gray, lang="eng", output_type=pytesseract.Output.DICT)
+data = pytesseract.image_to_data(gray, lang="eng+rus", output_type=pytesseract.Output.DICT)
 
 # Визуализация найденных слов
 for i in range(len(data["text"])):
@@ -24,3 +29,5 @@ for i in range(len(data["text"])):
 
 cv2.imwrite("document_result.png", img)
 print("✅ Готово: документ размечен и сохранён в document_result.png")
+
+pprint.pprint(data["text"]) #вывод всего распознанного текста из словаря
