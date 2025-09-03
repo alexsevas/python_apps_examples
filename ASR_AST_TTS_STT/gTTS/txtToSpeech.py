c4b99747172b01c9e0eda8d5da827fd87be193b4
alexsevas <mralexrelax@gmail.com>
2025-09-03 11:49:09 +0300

    Add comments

--- ASR_AST_TTS_STT/gTTS/txtToSpeech.py
+++ ASR_AST_TTS_STT/gTTS/txtToSpeech.py
@@ -1,4 +1,9 @@
-from gtts import gTTS 
+# conda activate allpy310, allpy311
+
+# pip install gTTS
+# online Работа - использует интернет-соединение
+
+from gtts import gTTS
 import os
 
 file = open("data/test.txt", "r").read()
@@ -6,5 +11,3 @@ file = open("data/test.txt", "r").read()
 speech = gTTS(text=file, lang='en', slow=False)
 speech.save("test.mp3")
 os.system("test.mp3")
-
-#print(file)
