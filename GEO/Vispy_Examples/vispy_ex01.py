7b44daf0649b6a0ff40ec41578afd3cbd93926f0
alexsevas <a1exsevas@yandex.ru>
2025-09-23 10:35:45 +0300

    Add vispy_ex01.py

---
+++ GEO/Vispy_Examples/vispy_ex01.py
@@ -0,0 +1,12 @@
+from vispy import app, visuals, scene
+import numpy as np
+
+canvas = scene.SceneCanvas(keys='interactive')
+view = canvas.central_widget.add_view()
+points = np.random.rand(10000, 3)
+scatter = scene.visuals.Markers()
+scatter.set_data(points, edge_color=None, face_color='white', size=7)
+view.add(scatter)
+view.camera = 'turntable'
+canvas.show()
+app.run()
