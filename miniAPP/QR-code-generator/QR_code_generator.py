4ad52429c60fc561b263b2685af77128e06f9a4e
alexsevas <a1exsevas@yandex.ru>
2025-10-14 22:41:10 +0300

    Add QR_code_generator.py

---
+++ miniAPP/QR-code-generator/QR_code_generator.py
@@ -0,0 +1,32 @@
+# conda activate allpy311
+# pip install qrcode
+
+'''
+Что делает:
+- Принимает строку (URL, текст и т.д.)
+- Генерирует QR-код с заданными параметрами (размер, граница)
+- Сохраняет в файл (PNG)
+'''
+
+import qrcode
+import sys
+
+def make_qr(data, output_file="qrcode.png", version=1, box_size=10, border=4):
+    qr = qrcode.QRCode(
+        version=version,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=box_size,
+        border=border,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(output_file)
+    print(f"QR saved to {output_file}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python QR_code_generator.py <text_or_url>")
+        sys.exit(1)
+    data = sys.argv[1]
+    make_qr(data)
