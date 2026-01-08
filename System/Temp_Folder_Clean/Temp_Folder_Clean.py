975348fad260198a7642213fabf0a00747e9519e
alexsevas <a1exsevas@yandex.ru>
2026-01-08 20:40:26 +0300

    Add Temp_Folder_Clean.py

---
+++ System/Temp_Folder_Clean/Temp_Folder_Clean.py
@@ -0,0 +1,35 @@
+# Windows-Temporary-File-Cleaner
+# A python script which will delete all windows temporary files and folders located inside following directory:
+# C:/Users/UserName/AppData/Local/Temp
+# Running^ python TempClean.py
+
+
+import os
+import shutil
+
+folder = 'C:/Users/ ' +os.getlogin( ) +'/AppData/Local/Temp'
+
+deleteFileCount = 0
+deleteFolderCount = 0
+
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    indexNo = file_path.find('\\')
+    itemName = file_path[indexNo +1:]
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+            print( '%s file deleted' % itemName )
+            deleteFileCount = deleteFileCount + 1
+
+        elif os.path.isdir(file_path):
+            if file_path.__contains__('chocolatey'):  continue
+            shutil.rmtree(file_path)
+            print( '%s folder deleted' % itemName )
+            deleteFolderCount = deleteFolderCount + 1
+
+    except Exception as e:
+        print('Access Denied: %s' % itemName )
+
+print(str(deleteFileCount ) +' files and  '+ str(deleteFolderCount ) +' folders deleted.')
+input('Press <Enter> to Exit')
