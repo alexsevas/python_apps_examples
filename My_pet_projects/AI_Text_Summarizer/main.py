8f37e99dbafef08b30470101e9250de8f217ad22
alexsevas <a1exsevas@yandex.ru>
2026-03-09 10:59:25 +0300

    Add main.py

---
+++ My_pet_projects/AI_Text_Summarizer/main.py
@@ -0,0 +1,20 @@
+# pip install PyQt5 python-dotenv openai google-generativeai g4f requests pypdf python-docx pymupdf
+
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = MainWindow()
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
